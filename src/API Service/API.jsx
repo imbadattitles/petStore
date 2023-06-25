14fd4ad044c67950ff12bf0e5baaa9e915fe3c76
Михаил <misha.odegoff@yandex.ru>
2023-06-25 12:47:39 +0500

    finally

--- src/API Service/API.jsx
+++ src/API Service/API.jsx
@@ -4,54 +4,51 @@ import { fetchUser, setAuth } from "../store/userReducer"
 
 export const fetchGoodsAction = async ({sort, search, category, page, dispatch}) => {
     dispatch(isLoading(true))
-    try {
-        fetch(`https://6475381ae607ba4797dbb5ea.mockapi.io/photos?${sort}&${search}&${category}&page=${page}&limit=6`).then(res => res.json()).then(json => {
+    fetch(`https://6475381ae607ba4797dbb5ea.mockapi.io/photos?${sort}&${search}&${category}&page=${page}&limit=6`).then(res => res.json())
+    .then(json => {
         dispatch(fetchGoods(json))
         dispatch(isLoading(false))
-        })
-    }
-    catch (e) {
-        console.log(e);
-    } 
+    })
+    .catch((e) => {
+        console.log(e)
+    })
 }
 
 export const fetchCategoriesAction = async ({dispatch}) => {
-    try {
         dispatch(isCategoriesLoading(true))
-        fetch('https://6475381ae607ba4797dbb5ea.mockapi.io/category').then(res => res.json()).then(json => {
-        dispatch(fetchCategories(json))
-        dispatch(isCategoriesLoading(false))
+        fetch('https://6475381ae607ba4797dbb5ea.mockapi.io/category').then(res => res.json())
+        .then(json => {
+            dispatch(fetchCategories(json))
+            dispatch(isCategoriesLoading(false))
+            })
+        .catch((e) => {
+            console.log(e)
         })
-    }
-    catch (e) {
-        console.log(e);
-    }
 }
 
 export const fetchItem = async ({id, dispatch, setItem}) => {
     dispatch(isLoading(true))
-    try {
-        fetch(`https://6475381ae607ba4797dbb5ea.mockapi.io/photos?search=${id}`).then(res => res.json()).then(json => {
+    fetch(`https://6dgfgdf475381ae607ba4797dbb5ea.mockapi.io/photos?search=${id}`).then(res => res.json())
+    .then(json => {
         setItem(json[0])
         dispatch(isLoading(false))
-        })
-    }  
-    catch (e) {
-        console.log(e);
-    } 
+    })
+    .catch((e) => {
+        console.log(e)
+    })
+    
 }
 
 export const fetchSimiliar = async ({item, dispatch, setSimilarGoods}) => {
     dispatch(isSimilarLoading(true)) 
-    try {
-        fetch(`https://6475381ae607ba4797dbb5ea.mockapi.io/photos?search=${item.category}`).then(res => res.json()).then(json => {
+    fetch(`https://6475381ae607ba4797dbb5ea.mockapi.io/photos?search=${item.category}`).then(res => res.json())
+    .then(json => {
         setSimilarGoods(json)
         dispatch(isSimilarLoading(false))
         })
-    }  
-    catch (e) {
+    .catch((e) => {
         console.log(e);
-    }  
+    })
 }
 
 export const FetchUsers = async ({dispatch, login, password, setError, router}) => {
